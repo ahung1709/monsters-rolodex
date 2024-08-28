cd0f1afd0dacba240b486758937da6366ac238b1
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2024-08-28 12:46:05 -0700

    add logic for filtering monsters

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
 
     this.state = {
       monsters: [], 
+      searchField: ''
     };
     console.log('constructor')
   }
@@ -30,11 +31,31 @@ class App extends Component {
       )
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
   render() {
     console.log('render')
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+    
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={onSearchChange}/>
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
